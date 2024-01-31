6fe6da27e27650ac8c6c1e9976498e7febed26f1
adamglupsk <adaari@icloud.com>
2024-01-31 16:31:21 +0100

    s

--- projects/test/script.js
+++ projects/test/script.js
@@ -1,28 +1,11 @@
-// Store the original width
-var originalWidth = '60px';
+function disableHover(button) {
+    var frDiv = button.closest('.fr');
 
-var frDiv = document.querySelector('.fr');
+    // Add a class to the "fr" div to disable hover effect
+    frDiv.classList.add('disable-hover');
 
-document.getElementById('Close').addEventListener('click', function () {
-    // Change the width
-    frDiv.style.width = '480px';
-
-    // Log a message to the console when changing the width
-    console.log('Width changed to 480px');
-
-    // Reset the width after a delay (adjust as needed)
+    // Remove the class after a delay (1 second in this example)
     setTimeout(function () {
-        frDiv.style.width = originalWidth;
-
-        // Log a message to the console when resetting the width
-        console.log('Width reset to original');
-    }, 200); // 1000 milliseconds = 1 second (adjust as needed)
-});
-
-// Add delayed event listener to handle hover effect after resetting the width
-setTimeout(function () {
-    frDiv.addEventListener('mouseover', function () {
-        // Reset the inline style to allow :hover to work again
-        this.style.width = '';
-    });
-}, 6000); // 500 milliseconds delay (adjust as needed)
+        frDiv.classList.remove('disable-hover');
+    }, 1000);
+}
