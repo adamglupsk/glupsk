dfdcaa776c78b6d09dbe8052b1e2fcd24c536661
adamglupsk <adaari@icloud.com>
2024-02-13 10:39:27 +0100

    a

---
+++ app.js
@@ -0,0 +1,43 @@
+// let x = 1;
+// let y = false;
+// let z = x + y;
+// console.log("%cy", "color: yellow;");
+
+// console.log("%clet x", "color: cyan;");
+// console.log("%clet y", "color: magenta;");
+// console.log("%c" + y + ' ' + x , "color: lime;");
+
+
+
+// let dynamicColor = "color: red;";
+// console.log("%c" + z, dynamicColor);
+
+// var css = 'color: red; font-size: 20px; background-color: yellow;';
+// console.log('%cHello World', css);
+
+// console.log(typeof x, typeof y, typeof z);
+let names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+let title = document.getElementById('title');
+
+
+
+if (names[1] ===( "John")) {
+    document.write("Hello John" + "<br>");
+    title.textContent = 'Hello World';
+}
+
+else    {
+    document.write("<div>You are not John</div>" + "<br>");
+}
+
+
+
+
+
+
+// let fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi', 'mango', 'pear', 'peach', 'plum', 'strawberry', 'watermelon'];
+// Iterating over the 'fruits' array
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i],  fruits[i]);
+// }
+
